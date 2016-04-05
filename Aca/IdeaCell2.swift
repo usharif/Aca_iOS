0df1c2eaf04699a4d802f561620085f8c7d1f77a
patron <patron@cs-wifiopen-117.cs.wisc.edu>
2016-04-04 19:08:22 -0500

    Fixed Issues after Merge Attempt

---
+++ Aca/IdeaCell2.swift
@@ -0,0 +1,26 @@
+//
+//  IdeaCell2.swift
+//  Aca
+//
+//  Created by patron on 4/4/16.
+//  Copyright © 2016 Umair Sharif. All rights reserved.
+//
+
+import UIKit
+
+class IdeaCell2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBOutlet weak var ideaName: UILabel!
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Aca/SongCell2.swift
@@ -0,0 +1,26 @@
+//
+//  SongCell2.swift
+//  Aca
+//
+//  Created by patron on 4/4/16.
+//  Copyright © 2016 Umair Sharif. All rights reserved.
+//
+
+import UIKit
+
+class SongCell2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    @IBOutlet weak var songName: UILabel!
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
