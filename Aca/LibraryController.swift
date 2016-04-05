c6d1b86c61e99da3bb8c224936c355af3b6b33b4
patron <patron@loan-col-046-x.local>
2016-04-05 15:43:03 -0500

    Added Navigation Titles to Each Table View Controller

---
+++ Aca/LibraryController.swift
@@ -0,0 +1,35 @@
+//
+//  LibraryController.swift
+//  Aca
+//
+//  Created by patron on 4/5/16.
+//  Copyright © 2016 Umair Sharif. All rights reserved.
+//
+
+import UIKit
+
+class LibraryController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
